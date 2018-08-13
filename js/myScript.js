ddb1e1d683805033d97187f5bed5201449d90719
TaviTeo <taviteo86@gmail.com>
2018-08-13 18:05:05 +0300

    13.08

---
+++ js/myScript.js
@@ -0,0 +1,11 @@
+function myFunction4() {
+    document.getElementById("demo4").innerHTML = "Paragraph changed.";
+}
+
+function myFunction5() {
+    document.getElementById("demo5").innerHTML = "Paragraph changed.";
+}
+
+function myFunction6() {
+    document.getElementById("demo6").innerHTML = "Paragraph changed.";
+}
